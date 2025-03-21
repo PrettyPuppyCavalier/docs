29490f4056688eb7380cd89a3b0ec0a0962ee6ef
sand <prettypuppycavalier@gmail.com>
2025-03-21 15:12:10 +0800

    add customerService

---
+++ customerService.js
@@ -0,0 +1,23 @@
+/**
+ * @name: customerService
+ * @author: sand
+ * @date: 2025-03-21 14:59
+ * @description：kefy
+ * @update: 2025-03-21 14:59
+ */
+window.difyChatbotConfig = {
+    token: 'sh6XCCISpQNdlw1U',
+    baseUrl: 'http://1.95.211.105'
+}
+// 创建 <script> 标签
+const script = document.createElement('script')
+
+// 设置 script 标签的属性
+script.src = 'http://1.95.211.105/embed.min.js'
+script.id = 'sh6XCCISpQNdlw1U'
+script.defer = true
+
+// 将 <script> 标签添加到 <body> 的末尾
+document.body.appendChild(script)
+
+
