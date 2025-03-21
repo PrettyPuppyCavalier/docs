c2dc9d97ad6dac2ba2cb29b9fb60b895f48ccf55
sand <prettypuppycavalier@gmail.com>
2025-03-21 16:15:22 +0800

    customerService

--- customerService.js
+++ customerService.js
@@ -5,17 +5,30 @@
  * @description：kefy
  * @update: 2025-03-21 14:59
  */
-window.difyChatbotConfig = {
-    token: 'sh6XCCISpQNdlw1U',
-    baseUrl: 'http://1.95.211.105'
-}
-// 创建 <script> 标签
-const script = document.createElement('script')
 
+const script = document.createElement('script')
 // 设置 script 标签的属性
-script.src = './embed.min.js'
-script.id = 'sh6XCCISpQNdlw1U'
-script.defer = true
+script.src = 'https://sf-cdn.coze.com/obj/unpkg-va/flow-platform/chat-app-sdk/1.2.0-beta.5/libs/oversea/index.js'
+
+// 监听 script 加载完成事件
+script.onload = function () {
+    new CozeWebSDK.WebChatClient({
+        config: {
+            bot_id: '7484166014775967752',
+        },
+        componentProps: {
+            title: 'Coze',
+        },
+        auth: {
+            type: 'token',
+            token: 'pat_********',
+            onRefreshToken: function () {
+                return 'pat_********';
+            }
+        }
+    });
+};
 
 // 将 <script> 标签添加到 <body> 的末尾
 document.body.appendChild(script)
+
